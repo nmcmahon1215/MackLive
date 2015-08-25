e1e192eacca625dbcd3b03987598bbc8104f4884
Nick McMahon <npm.mcmahon@gmail.com>
2015-08-25 14:09:27 -0400

    Converts tabs to spaces

--- src/com/macklive/rest/JavaScriptService.java
+++ src/com/macklive/rest/JavaScriptService.java
@@ -1,3 +1,6 @@
+/**
+ * This class returns the applicable javascript when loading pages.
+ */
 package com.macklive.rest;
 
 import java.io.BufferedReader;
@@ -15,74 +18,74 @@ import javax.ws.rs.core.Context;
 @Path("/js")
 public class JavaScriptService {
 
-	/**
-	 * Gets the common JavaScript files and returns them.
-	 * @param context ServletContext used for finding the JS files
-	 * @return The combined JavaScript
-	 * @throws IOException if the files cannot be read.
-	 */
-	@GET
-	@Produces("text/javascript")
-	public String getBaseJavaScript(@Context ServletContext context) throws IOException {
-		String result = "";
+    /**
+     * Gets the common JavaScript files and returns them.
+     * @param context ServletContext used for finding the JS files
+     * @return The combined JavaScript
+     * @throws IOException if the files cannot be read.
+     */
+    @GET
+    @Produces("text/javascript")
+    public String getBaseJavaScript(@Context ServletContext context) throws IOException {
+        String result = "";
 
-		//Get Common Files
-		String pathName = context.getRealPath("/js/common");
-		
-		if (pathName != null){
-			File commonFiles = new File(context.getRealPath("/js/common"));
-			for (File f : commonFiles.listFiles()){
-				result += this.getFileString(f);
-			}
-		}
+        //Get Common Files
+        String pathName = context.getRealPath("/js/common");
 
-		return result;
-	}
+        if (pathName != null){
+            File commonFiles = new File(context.getRealPath("/js/common"));
+            for (File f : commonFiles.listFiles()){
+                result += this.getFileString(f);
+            }
+        }
 
-	/**
-	 * Gets common JavaScript files and those in the given subfolder.
-	 * @param subFolder The subfolder to search for.
-	 * @param context Servlet context for finding the JS files.
-	 * @return The combined JavaScript.
-	 * @throws IOException
-	 */
-	@GET
-	@Path("/{subFolder}")
-	@Produces("text/javascript")
-	public String getAdditionalJavaScript(@PathParam("subFolder") String subFolder, @Context ServletContext context) throws IOException{
-		String result = this.getBaseJavaScript(context);
+        return result;
+    }
 
-		//Get additional files, if any
-		if (subFolder != null && !subFolder.isEmpty()){
-			String pathName = context.getRealPath("/js/" + subFolder);
-			if (pathName != null){
-				File requestedFiles = new File(context.getRealPath("/js/" + subFolder));
-				for (File f : requestedFiles.listFiles()){
-					result += this.getFileString(f);
-				}
-			}
-		}
+    /**
+     * Gets common JavaScript files and those in the given subfolder.
+     * @param subFolder The subfolder to search for.
+     * @param context Servlet context for finding the JS files.
+     * @return The combined JavaScript.
+     * @throws IOException
+     */
+    @GET
+    @Path("/{subFolder}")
+    @Produces("text/javascript")
+    public String getAdditionalJavaScript(@PathParam("subFolder") String subFolder, @Context ServletContext context) throws IOException{
+        String result = this.getBaseJavaScript(context);
 
-		return result;
-	}
+        //Get additional files, if any
+        if (subFolder != null && !subFolder.isEmpty()){
+            String pathName = context.getRealPath("/js/" + subFolder);
+            if (pathName != null){
+                File requestedFiles = new File(context.getRealPath("/js/" + subFolder));
+                for (File f : requestedFiles.listFiles()){
+                    result += this.getFileString(f);
+                }
+            }
+        }
 
-	/**
-	 * Returns a string version of the contents of a file
-	 * @param f File to stringify
-	 * @return A string containing the file's contents
-	 * @throws IOException if I/O error occurs
-	 */
-	private String getFileString(File f) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader(f));
-		String line = br.readLine();
-		String contents = "";
-		while(line != null){
-			contents += line;
-			contents += "\n";
-			line = br.readLine();
-		}
-		br.close();
-		contents += "\n";
-		return contents;
-	}
+        return result;
+    }
+
+    /**
+     * Returns a string version of the contents of a file
+     * @param f File to stringify
+     * @return A string containing the file's contents
+     * @throws IOException if I/O error occurs
+     */
+    private String getFileString(File f) throws IOException {
+        BufferedReader br = new BufferedReader(new FileReader(f));
+        String line = br.readLine();
+        String contents = "";
+        while(line != null){
+            contents += line;
+            contents += "\n";
+            line = br.readLine();
+        }
+        br.close();
+        contents += "\n";
+        return contents;
+    }
 }
