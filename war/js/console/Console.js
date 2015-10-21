428cdcc13cc1ae8a834ed2107b52a9fc53ba299c
Nick McMahon <npm.mcmahon@gmail.com>
2015-10-20 22:13:02 -0400

    Updates console with new functions for games

--- war/js/console/Console.js
+++ war/js/console/Console.js
@@ -14,10 +14,52 @@ Console = function() {
 Console.prototype = {
 	initialize: function() {
 		this.setGameName("N/A");
+		this.loadRecentGames();
+		
+		$j(this.createButton).click(this.createGame.bind(this));
 	},
 
 	setGameName: function (name) {
 		this.gameName = name;
 		this.titleElement.innerHTML = "Current Game: " + this.gameName;
 	},
+	loadRecentGames: function() {
+		$j.ajax({
+			url: location.protocol + '//' + location.host + "/api/game/recent/5",
+			context: this,
+			success: function(response) {
+				var games = response.split(",");
+				this.gamePicker.innerHTML = "<option>Select A Game...</option>";
+				games.forEach(function (game){
+					if (game != ""){
+						this.gamePicker.innerHTML += "<option>" + game + "</option>";
+					}
+				});
+			},
+			failure: function(response) {
+				alert("Could not fetch recent games.");
+			}
+		})
+	},
+	createGame: function () {
+		var today = new Date();
+		var team1 = document.getElementById("team1select").value;
+		var team2 = document.getElementById("team2select").value;
+		
+		$j.ajax({
+			url:location.protocol + '//' + location.host + "/api/game",
+			method: "POST",
+			contentType: "text/plain",
+			dataType: "application/json",
+			data: team1 + "," + team2,
+			context: this,
+			success: function(response){
+				this.gameId = response.id;
+				this.setGameName(response.name);
+			},
+			failure: function (response) {
+				alert("Failed to create new game.");
+			},
+		})
+	}
 }
