ccc5d9a976acd193e112e604f56a9978129fcc71
Nick McMahon <npm.mcmahon@gmail.com>
2015-10-20 23:20:29 -0400

    Adjusts JSONUtility to produce more valid output

--- src/com/macklive/utility/JSONUtility.java
+++ src/com/macklive/utility/JSONUtility.java
@@ -1,8 +1,13 @@
 package com.macklive.utility;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.macklive.objects.IBusinessObject;
 
 /**
  * A class for quickly creating JSON strings as return values for REST calls
@@ -16,12 +21,42 @@ public class JSONUtility {
     }
     
     /**
+     * Returns a JSON Utility representation of the given object.
+     * @param obj Object to convert into a JSONUtility
+     * @return A JSONUtiltiy filled with the appropriate properties to represent the object.
+     */
+    public static JSONUtility build(IBusinessObject obj){
+        Entity e = obj.getEntity();
+        JSONUtility jsu = new JSONUtility();
+        for (String field : e.getProperties().keySet()){
+            jsu.addProperty(field, e.getProperty(field));
+        }
+        jsu.addProperty("id", e.getKey().getId());
+        return jsu;
+    };
+    
+    /**
+     * Overrides getJSON to allow specifying a Key. This avoids the need 
+     * to lookup an item in the data store in some cases.
+     * @param obj Object to convert into a JSONUtility
+     * @param key Key used as an identifier for the object in the data store.
+     * @return A JSONUtility filled with the appropriate properties to represent the Object.
+     */
+    public static JSONUtility build(IBusinessObject obj, Key key){
+        JSONUtility jsu = JSONUtility.build(obj);
+        jsu.addProperty("id", key.getId());
+        return jsu;
+    }
+    
+    /**
      * Adds a property to the JSON object.
      * @param key Key for the object.
      * @param value Value for the given key
      */
     public void addProperty(String key, Object value){
-        values.put(key, value);
+        if (value != null){
+            values.put(key, value);
+        }
     }
     
     /**
@@ -34,8 +69,15 @@ public class JSONUtility {
         for (String key : keyset){
             result += "\"" + key + "\":";
             Object val = values.get(key);
-            result += "\"" + val.toString() + "\",";
+            if (val instanceof JSONUtility || val instanceof Collection){
+                result += val.toString();
+            } else {
+                result += "\"" + val.toString() + "\"";
+            }
+            
+            result += ", ";
         }
+        result = result.substring(0, result.length() - 2);
         result += " }";
         return result;
     }
