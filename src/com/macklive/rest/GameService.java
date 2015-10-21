d1a2a04ab5bed286a8bb13d4a00a6cecdb182605
Nick McMahon <npm.mcmahon@gmail.com>
2015-10-20 22:12:51 -0400

    Adds new GameService. Adds JSON utility for quickly making JSON objects for REST call returns

---
+++ src/com/macklive/rest/GameService.java
@@ -0,0 +1,62 @@
+package com.macklive.rest;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.appengine.api.datastore.Key;
+import com.macklive.objects.Game;
+import com.macklive.objects.Team;
+import com.macklive.storage.DataManager;
+import com.macklive.utility.JSONUtility;
+
+/**
+ * Rest endpoint for getting Game information
+ */
+@Path("/game")
+public class GameService {
+
+    @GET
+    @Path("/recent/{num}")
+    public String getRecentGames(@PathParam("num") int number){
+        List<Game> games = DataManager.getInstance().getRecentGames(number);
+        String result = "";
+        
+        for (Game g : games){
+            result += g.toString();
+            result += ",";
+        }
+        
+        return result;
+    }
+    
+    @POST
+    @Consumes(MediaType.TEXT_PLAIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String createNewGame(String teams){
+        String[] teamNames = teams.split(",");
+        String team1Name = teamNames[0];
+        String team2Name = teamNames[1];
+        
+        DataManager dstore = DataManager.getInstance();
+        Team t1 = dstore.getTeamByName(team1Name);
+        Team t2 = dstore.getTeamByName(team2Name);
+        
+        Game g = new Game(t1, t2);
+        
+        Key k = dstore.storeItem(g);
+        
+        JSONUtility jsu= new JSONUtility();
+        jsu.addProperty("id", k.getId());
+        jsu.addProperty("name", g);
+        
+        return jsu.getJSON()3;
+    }
+}
---
+++ src/com/macklive/utility/JSONUtility.java
@@ -0,0 +1,42 @@
+package com.macklive.utility;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * A class for quickly creating JSON strings as return values for REST calls
+ */
+public class JSONUtility {
+
+    Map<String, Object> values;
+    
+    public JSONUtility(){
+        values = new HashMap<String, Object>();
+    }
+    
+    /**
+     * Adds a property to the JSON object.
+     * @param key Key for the object.
+     * @param value Value for the given key
+     */
+    public void addProperty(String key, Object value){
+        values.put(key, value);
+    }
+    
+    /**
+     * Generates the JSON string.
+     * @return A JSON representation of the Keys and values of the map
+     */
+    public String getJSON(){
+        Set<String> keyset = values.keySet();
+        String result = "{ ";
+        for (String key : keyset){
+            result += "\"" + key + "\":";
+            Object val = values.get(key);
+            result += "\"" + val.toString() + "\",";
+        }
+        result += " }";
+        return result;
+    }
+}
