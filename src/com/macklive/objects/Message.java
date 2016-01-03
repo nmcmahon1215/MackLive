de1b39e166910bc919f2b2a46f9d7daa174405d0
Nick McMahon <npm.mcmahon@gmail.com>
2016-01-03 00:11:20 -0500

    Adds comments and a getter to Message

--- src/com/macklive/objects/Message.java
+++ src/com/macklive/objects/Message.java
@@ -16,6 +16,18 @@ public class Message extends AbsBusinessObject {
     private long gameId;
     private boolean approved;
 
+    /**
+     * Constructor.
+     * 
+     * @param author
+     *            Author of the message
+     * @param text
+     *            Text of the message
+     * @param gameId
+     *            ID of the game the message belongs to.
+     * @param approved
+     *            Whether or not the message is approved for public use
+     */
     public Message(String author, String text, long gameId, boolean approved) {
         this.author = author;
         this.text = text;
@@ -24,6 +36,14 @@ public class Message extends AbsBusinessObject {
         this.gameId = gameId;
     }
 
+    /**
+     * Alternate constructor
+     * 
+     * @param e
+     *            Entity from the data store to load the object from.
+     * @throws EntityMismatchException
+     *             if the Entity is of the wrong type.
+     */
     public Message(Entity e) throws EntityMismatchException {
         this.loadEntity(e);
     }
@@ -63,7 +83,21 @@ public class Message extends AbsBusinessObject {
         }
     }
 
+    /**
+     * Gets the time of the message
+     * 
+     * @return Time of the message
+     */
     public Date getTime() {
         return time;
+    }
+
+    /**
+     * Getter for game id.
+     * 
+     * @return GameID which this message belongs to.
+     */
+    public long getGameId() {
+        return gameId;
     }
 }
