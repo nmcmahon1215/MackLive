bd1043cb7a43041fff643fb2cf49546c62638353
Nick McMahon <npm.mcmahon@gmail.com>
2016-06-26 16:04:28 -0400

    Adds embed modal

--- war/js/console/Modal.js
+++ war/js/console/Modal.js
@@ -11,6 +11,24 @@ Modal = function () {
         this.focus();
         this.select();
     });
+
+    this.widthBox = document.getElementById("embedWidth");
+    this.heightBox = document.getElementById("embedHeight");
+
+    this.widthBox.value = this.width;
+    this.heightBox.value = this.height;
+
+    //Event Handlers
+    var self = this;
+
+    $j(this.widthBox).on("change", function () {
+        self.setWidth(this.value)
+    });
+
+    $j(this.heightBox).on("change", function () {
+        self.setHeight(this.value)
+    });
+
 }
 
 Modal.prototype = {
